e98581a7f45d621a18cd57711aa3a6e502a756bb
Stefan Kolb <stefan-kolb@users.noreply.github.com>
2020-11-06 18:10:43 +0100

    chore: Adapt to Cloud 9 environment

--- web/app.js
+++ web/app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.set('port', process.env.PORT || 8080);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
